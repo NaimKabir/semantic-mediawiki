67c10db8e20d570ed395a9d5d006acf04171f354
NaimKabir <kabir.naim@gmail.com>
2021-09-03 20:15:05 +0400

    [Templated Bazel] Templating build files so pinned versions are configurable.

---
+++ gen.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+
+TEMPLATES_DIR = "templates"
+
+
+def get_template_render(path: str) -> str:
+    """
+    Render a Jinja2 template to actual text, using macros in scope.
+
+    :param path: Path to the file you'd like to render.
+    """
+    env = Environment(loader=FileSystemLoader("."))
+    render = env.from_string(open(path, "r").read()).render()
+    return render
+
+
+def gen(templates_dir: str) -> str:
+    """
+    Crawl the templates directory and generate files in the correct locations.
+    NOTE: This must be run from project root!
+
+    :param templates_dir: The directory of templates, which mirror project root.
+    """
+
+    for root, _, files in os.walk(templates_dir, topdown=True):
+        for filename in files:
+            # TEMPLATES_DIR should act a mirror of the codebase, so the filepaths
+            # of each template file should be the same as the filepaths of template targets.
+            template_file = Path(root) / filename
+            target_file = str(template_file).replace(".jinja", "").replace(templates_dir + "/", "")
+            rendered_template = render(template_file)
+            with open(target_file, "w") as f:
+                f.write(rendered_template)
+
+
+if __name__ == "__main__":
+    gen(TEMPLATES_DIR)
